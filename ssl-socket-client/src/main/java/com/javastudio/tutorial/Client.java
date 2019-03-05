20278c1f935449da57c05b650beb9105b893b489
Hossein Mohammadi <hussein.muhammadii@gmail.com>
2019-03-05 13:06:14 +0330

    SSL Client

---
+++ ssl-socket-client/src/main/java/com/javastudio/tutorial/Client.java
@@ -0,0 +1,59 @@
+package com.javastudio.tutorial;
+
+import javax.net.ssl.SSLSocket;
+import javax.net.ssl.SSLSocketFactory;
+import java.io.*;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+public class Client {
+    public static void main(String[] args) {
+        System.setProperty("javax.net.ssl.trustStore", "keystore.jks");
+        System.setProperty("javax.net.ssl.trustStorePassword", "123456");
+        System.setProperty("javax.net.ssl.keyStore", "keystore.jks");
+        System.setProperty("javax.net.ssl.keyStorePassword", "123456");
+
+        String host = "localhost";
+        if (args != null && args.length > 0)
+            host = args[0];
+
+        int port = 12400;
+        if (args != null && args.length > 1)
+            port = Integer.parseInt(args[1]);
+
+        try {
+            System.out.println("Connecting to " + host + " on port " + port);
+            Socket socket = new Socket(host, port);
+            socket = SSLSocketFactory.getDefault().createSocket();
+            socket.setKeepAlive(true);
+            socket.setSoLinger(true, 0);
+            socket.setReuseAddress(true);
+            socket.setTcpNoDelay(true);
+            socket.bind(new InetSocketAddress(new InetSocketAddress(port).getAddress(), port));
+            socket.connect(new InetSocketAddress(host, port));
+            ((SSLSocket) socket).setEnabledProtocols(new String[]{"TLSv1.2"});
+            ((SSLSocket) socket).startHandshake();
+
+            socket.setKeepAlive(true);
+            socket.setTcpNoDelay(true);
+            socket.getInputStream();
+            socket.getOutputStream();
+            socket.setSoLinger(true, 0);
+
+            System.out.println("Just connected to " + socket.getRemoteSocketAddress());
+            OutputStream outToServer = socket.getOutputStream();
+            DataOutputStream out = new DataOutputStream(outToServer);
+
+            out.writeUTF("Hello from " + socket.getLocalSocketAddress());
+            InputStream inFromServer = socket.getInputStream();
+            DataInputStream in = new DataInputStream(inFromServer);
+
+            System.out.println("Server says " + in.readUTF());
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
